3ca788c83ea021904c41675e08a93e71b5bf694c
legomenon <solomovsky.roman@gmail.com>
2022-07-08 20:54:45 +0300

    add database connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/jackc/pgx/v4"
 )
 
 type application struct {
@@ -14,9 +18,16 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "postgres://root:secret@localhost/goSnpDB?sslmode=disable", "Postgres data source dsn")
 	flag.Parse()
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatalf("Unable to connect to database: %v\n", err)
+	}
+	defer db.Close(context.Background())
 
 	app := &application{
 		errorLog: errorLog,
@@ -30,6 +41,22 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s\n", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*pgx.Conn, error) {
+	conn, err := pgx.Connect(context.Background(), dsn)
+	if err != nil {
+		return nil, err
+	}
+	// defer conn.Close(context.Background())
+
+	err = conn.Ping(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("connect to postgres")
+
+	return conn, nil
 }
