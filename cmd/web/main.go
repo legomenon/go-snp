4329c89a789b586245b0e84392139470d05a1f73
legomenon <solomovsky.roman@gmail.com>
2022-07-15 14:04:39 +0300

    add sessions

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"go-snp/internal/models"
@@ -9,15 +10,21 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
+	"github.com/alexedwards/scs/postgresstore"
+	"github.com/alexedwards/scs/v2"
 	"github.com/jackc/pgx/v4/pgxpool"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
 type application struct {
-	errorLog      *log.Logger
-	infoLog       *log.Logger
-	snippets      *models.SnippetModel
-	templateCache map[string]*template.Template
+	errorLog       *log.Logger
+	infoLog        *log.Logger
+	snippets       *models.SnippetModel
+	templateCache  map[string]*template.Template
+	sessionManager *scs.SessionManager
 }
 
 func main() {
@@ -27,22 +34,28 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	db, err := openDB(*dsn)
+	db, nativeDB, err := openDB(*dsn)
 	if err != nil {
 		errorLog.Fatalf("Unable to connect to database: %v\n", err)
 	}
 	defer db.Close()
+	defer nativeDB.Close()
 
 	templateCache, err := newTemplateCache()
 	if err != nil {
 		errorLog.Fatal(err)
 	}
 
+	sessionManager := scs.New()
+	sessionManager.Store = postgresstore.New(nativeDB)
+	sessionManager.Lifetime = 12 * time.Hour
+
 	app := &application{
-		errorLog:      errorLog,
-		infoLog:       infoLog,
-		snippets:      &models.SnippetModel{DB: db},
-		templateCache: templateCache,
+		errorLog:       errorLog,
+		infoLog:        infoLog,
+		snippets:       &models.SnippetModel{DB: db},
+		templateCache:  templateCache,
+		sessionManager: sessionManager,
 	}
 
 	srv := &http.Server{
@@ -56,17 +69,31 @@ func main() {
 	errorLog.Fatal(err)
 }
 
-func openDB(dsn string) (*pgxpool.Pool, error) {
+func openDB(dsn string) (*pgxpool.Pool, *sql.DB, error) {
 	conn, err := pgxpool.Connect(context.Background(), dsn)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
-	err = conn.Ping(context.Background())
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = conn.Ping(ctx)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 	fmt.Println("connect to postgres")
 
-	return conn, nil
+	nativeDB, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	err = nativeDB.PingContext(ctx)
+	if err != nil {
+		return nil, nil, err
+	}
+	fmt.Println("native connect to postgres")
+
+	return conn, nativeDB, nil
 }
